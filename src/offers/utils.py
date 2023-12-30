3f9f705e4d504bdcd993e6629c518b6b3bb18c3e
DmytroKuriptya <dima343782@gmail.com>
2023-12-30 19:12:04 +0200

    optimized business logic, developed functionality that fetches data from a site and gives the data to its handler function

--- src/offers/utils.py
+++ src/offers/utils.py
@@ -1,34 +1,41 @@
 import asyncio
 import aiohttp
+from bs4 import BeautifulSoup
 
 
-rent_sites_links = {
-    'olx': 'https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/kiev/?currency=UAH',
-    'dim_ria': 'https://dom.ria.com/uk/arenda-kvartir/kiev/',
-    'rieltor': 'https://rieltor.ua/kiev/flats-rent/'
+def olx_handler(data):
+    return 'olx'
+
+
+def dim_ria_handler(data):
+    return 'dim_ria'
+
+
+def rieltor_handler(data):
+    return 'rieltor'
+
+
+rent_sites = {
+    'olx': ('https://www.olx.ua/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/kiev/?currency=UAH', olx_handler),
+    'dim_ria': ('https://dom.ria.com/uk/arenda-kvartir/kiev/', dim_ria_handler),
+    'rieltor': ('https://rieltor.ua/kiev/flats-rent/', rieltor_handler)
 }
 
 
-async def olx_handler(url, session):
-    return 'olx response'
-
-
-async def dim_ria_handler(url, session):
-    return 'dim ria response'
-
-
-async def rieltor_handler(url, session):
-    return 'rieltor response'
+async def fetch_data(url, handler, session):
+    async with session.get(url) as response:
+        result = await response.text()
+    raw_data = BeautifulSoup(result)
+    ready_data = handler(raw_data)
+    return ready_data
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        cors = [
-            olx_handler(rent_sites_links['olx'], session),
-            dim_ria_handler(rent_sites_links['dim_ria'], session),
-            rieltor_handler(rent_sites_links['rieltor'], session)
-        ]
-        res = await asyncio.gather(*cors)
+        tasks = []
+        for site in rent_sites:
+            tasks.append(fetch_data(rent_sites[site][0], rent_sites[site][1], session))
+        res = await asyncio.gather(*tasks)
     return ' | '.join(res)
 
 
